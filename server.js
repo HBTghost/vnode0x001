0352c0c0f6d69e544068e596c53451a7b399bb51
Brian Drake <hacbangtam@gmail.com>
2020-05-01 22:41:14 +0700

    Test get api from local

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+// Load env
+dotenv.config({ path: "./process.env" });
+
+const app = express();
+
+// Dev logging
+if (process.env.NODE_ENV === "dev") {
+  app.use(morgan("dev"));
+}
+
+// Routes
+app.get("/api/:place/:date", (req, res) => {
+  const { place, date } = req.params;
+  res.send(`${place} - ${date}`);
+})
+
+const port = process.env.PORT || 7070;
+
+app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+})
