3670413e6ac1b3bef2ae7d1411f494866b69e0e7
Brian Drake <hacbangtam@gmail.com>
2020-05-07 12:27:43 +0700

    Test heroku deploy

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ if (process.env.NODE_ENV === "dev") {
 app.use("/api/genDateStrings", require("./routes/genDateStrings.js"))
 app.use("/api/result", require("./routes/result.js"));
 
+// Handle production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(__dirname + "/public/"));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
 // Choose port
 const port = process.env.PORT || 7070;
 
