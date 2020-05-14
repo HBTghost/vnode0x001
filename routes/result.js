838c2bd74f7334fc31f1df3a5d925335de8cf629
Brian Drake <hacbangtam@gmail.com>
2020-05-14 16:02:54 +0700

    test new send

--- routes/result.js
+++ routes/result.js
@@ -3,15 +3,16 @@ const router = express.Router();
 
 router.get("/:place/:date", async (req, res) => {
   try {
-    const { place, date } = req.params;
-    var history;
+    // const { place, date } = req.params;
+    // var history;
 
-    if (place === "north") {
-      history = require("./history/north.js");
-    } else {
-      history = require("./history/other.js");
-    }
-    const data = await history.getResult(place, date);
+    // if (place === "north") {
+    //   history = require("./history/north.js");
+    // } else {
+    //   history = require("./history/other.js");
+    // }
+    // const data = await history.getResult(place, date);
+    const data = require("./data/res_south_07_05_2020.json");
     res.json(data);
   } catch(err) {
     console.error(err);
