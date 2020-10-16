c338cf09129c7ca3bf8687601126d8270e00d222
Brian Drake <hacbangtam@gmail.com>
2020-10-16 16:30:08 +0700

    Change ?? to ||

--- server/tools/results.js
+++ server/tools/results.js
@@ -5,7 +5,7 @@ async function getLottery(req, res, next) {
     const lottery = new Lottery(req.params.region, req.params.date);
     res.lottery = await lottery.getOrUpdate();
   } catch(err) {
-    return res.status(err.status ?? 500).json({ message: err.message });
+    return res.status(err.status || 500).json({ message: err.message });
   }
 
   next();
